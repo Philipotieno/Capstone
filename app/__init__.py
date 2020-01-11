72c3bb7d2f3da04793734fd9bc9ec62409337471
Philipotieno <tmitchellb007@gmail.com>
2020-01-11 05:50:03 +0300

    chore: adds route to test the app

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# local import
+from instance.config import app_config
+
+# initialize sql-alchemy
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    """
+    flask Object and returns it after it is loaded
+    with the configurations settings
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    return app
